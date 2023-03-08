7729f6097e8d2fcd4557f0cd7eb4dea30c2ddd87
Sillas Gabriel Coelho Sena <SillasGCS@academicos.com>
2023-03-08 20:49:40 -0300

    atualizacao

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # HTML-CSS-AND-JS
 Sites testes.
+oi
---
+++ site de 3 img/java.js
@@ -0,0 +1,11 @@
+function irimg1() {
+    window.alert('Imagem-1')
+}
+
+function irimg2() {
+    window.alert('Imagem-2')
+}
+
+function irimg3() {
+    window.alert('Imagem-3')
+}
